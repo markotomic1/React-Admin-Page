4d0a9ae58adaaf3a3b937c5f80d080756b1489d5
Marko <tomicmark3@gmail.com>
2022-11-03 15:10:10 +0100

    final commit

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,7 @@
 import "./topbar.css";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LanguageIcon from "@mui/icons-material/Language";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
 function Topbar() {
   return (
@@ -7,7 +10,24 @@ function Topbar() {
         <div className='topLeft'>
           <span className='logo'>Admin</span>
         </div>
-        <div className='topRight'>right</div>
+        <div className='topRight'>
+          <div className='topbarIcons'>
+            <NotificationsNoneIcon />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className='topbarIcons'>
+            <LanguageIcon />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className='topbarIcons'>
+            <SettingsOutlinedIcon />
+          </div>
+          <img
+            src='https://cdn.pixabay.com/photo/2016/12/26/18/33/logo-1932539__340.png'
+            alt=''
+            className='topAvatar'
+          />
+        </div>
       </div>
     </div>
   );
